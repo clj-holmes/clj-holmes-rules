d3ce0e44749dad47e786a059baf20d16d92e9b75
rancorzinho <38864720+rancorzinho@users.noreply.github.com>
2022-02-01 14:30:31 -0300

    fix rule and add test

---
+++ security/deprecated-blowfish/deprecated-blowfish.clj
@@ -0,0 +1,8 @@
+(ns deprecated-blowfish
+  (:import [javax.crypto Cipher]))
+
+(defn vulnerable []
+  (Cipher/getInstance "Blowfish/CBC/PKCS5Padding"))
+
+(defn not-vulnerable []
+  (Cipher/getInstance "RSA/ECB/PKCS1Padding"))
